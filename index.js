9e12ab46f5fe2c077991f8ae31b7d2a60a723890
Vishnu Prabhakar <vishnuprabhakar42@gmail.com>
2025-08-10 11:29:01 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from GitHub Actions Test App 🚀");
+  res.send("Hello from GitHub Actions Test App and this is the update  🚀");
 });
 
 const PORT = process.env.PORT || 3000;
