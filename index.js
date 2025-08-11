cc2aa23d959557f5de8fc3dae42315292514a3c8
vishnuprabhakar42 <63367697+vishnuprabhakar42@users.noreply.github.com>
2025-08-11 15:09:43 +0530

    Update index.js
    changes in indes.js file, not a mojor change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from GitHub Actions Test App 🚀");
+  res.send("Hello from GitHub Actions Test App, it is only for testing purpose 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
